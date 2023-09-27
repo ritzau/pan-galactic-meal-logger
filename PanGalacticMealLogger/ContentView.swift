6a34c67d36faa9de482495b170db490534501cae
Tobias Ritzau <tobias@ritzau.se>
2023-09-28 00:21:26 +0200

    Initial Commit

---
+++ PanGalacticMealLogger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PanGalacticMealLogger
+//
+//  Created by Tobias Ritzau on 2023-09-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PanGalacticMealLogger/PanGalacticMealLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanGalacticMealLoggerApp.swift
+//  PanGalacticMealLogger
+//
+//  Created by Tobias Ritzau on 2023-09-28.
+//
+
+import SwiftUI
+
+@main
+struct PanGalacticMealLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
